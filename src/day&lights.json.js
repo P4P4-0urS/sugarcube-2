e9ebc9b179c03cf3c9241bece2da72c04bd89c6c
P4P4-0urS <38294663+P4P4-0urS@users.noreply.github.com>
2020-10-12 18:54:55 +0200

    casse

--- src/day&lights.json.js
+++ src/day&lights.json.js
@@ -1,7 +1,12 @@
-let cssDayLights = (() => { return Object.freeze(Object.defineProperties({}, { css : { value : {
-".btn-switch-bg-dayLights" : { 
+let cssDayLights = (() => { return Object.freeze(Object.defineProperties({}, { css : { value : {" 
+.btn-switch-bg-dayLights" : { 
 "background-color" : ["#cccccc","#6d6d6d"]
 },
-".btn-switch-bg-dayLights:before" : { 
+" .btn-switch-bg-dayLights:before" : { 
 "background-color" : ["#ffffff","#222"]
+}
+" .table-secondary,
+.table-secondary > th,
+.table-secondary > td" : { 
+"background-color" : ["#fdfefe","
 }}}}))})();
